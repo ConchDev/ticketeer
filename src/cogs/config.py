6cc63995680c450305978ac245e6cd3c7249b1a6
ConchDev <unsoughtconch@gmail.com>
2022-11-19 14:36:38 -0500

    feat: create set channel command

--- src/cogs/config.py
+++ src/cogs/config.py
@@ -1,5 +1,11 @@
 import discord
 import typing
+from database.models import (
+    Guild,
+    Ticket,
+    TicketMessage,
+    TicketUser
+)
 
 if typing.TYPE_CHECKING:
     from objects import Bot
@@ -8,6 +14,21 @@ if typing.TYPE_CHECKING:
 class Config(discord.Cog):
     def __init__(self, bot: "Bot"):
         self.bot = bot
+    
+    set_cmd = discord.SlashCommandGroup(
+        name="set",
+        description="Configure Ticketeer settings.",
+        checks=[lambda ctx: ctx.author.guild_permissions.manage_guild]
+        )
+
+    @set_cmd.command(name="channel")
+    async def command_set_channel(ctx: discord.ApplicationContext, channel: discord.TextChannel):
+        """
+        Set the channel where tickets are handled.
+        """
+        guild = await Guild.get_or_create(id=ctx.guild.id)
+        await guild.update(ticket_channel=channel.id)
+        await ctx.send(f"Ticket channel set to {channel.mention}.")
     
 
 def setup(bot: "Bot"):
